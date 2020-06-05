2ad1012c084be237f818f0537b52d0db2ee2d520
Kate <grapentinekate@gmail.com>
2020-06-04 19:40:45 -0700

    Improved my code to include let and const as variables.

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
-var retailItemPrice = 9.99;
-var wholesaleItemPrice = 6.25;
+//declaring my variables:
 
-var myWalletAmount = 20.00;
-var brotherWalletAmount = 50.25;
+let retailItemPrice = 9.99;
+let wholesaleItemPrice = 6.25;
 
-var myNumberOfFriends = 65;
-var brotherNumberOfFriends = 81;
+let myWalletAmount = 20.00;
+let brotherWalletAmount = 50.25;
 
-var myAge = 22
-var brotherAge = 26
+let myNumberOfFriends = 65;
+let brotherNumberOfFriends = 81;
 
-var myFirstName = 'Kate';
-var brotherFirstName = 'Riley';
+let myAge = 22
+let brotherAge = 26
 
-var maidenLastName = 'Grapentine';
-var adoptedLastName = 'Green';
+const myFirstName = 'Kate';
+const brotherFirstName = 'Riley';
 
-var myMidInitial = 'L';
-var brotherMidInitial = 'M';
+const maidenLastName = 'Grapentine';
+const adoptedLastName = 'Green';
+
+const myMidInitial = 'L';
+const brotherMidInitial = 'M';
 
 //varibles that preform operations:
 
-var newMyWalletAmount = myWalletAmount - retailItemPrice;
-var newBrotherWalletAmount = brotherWalletAmount - wholesaleItemPrice;
+let newMyWalletAmount = myWalletAmount - retailItemPrice;
+let newBrotherWalletAmount = brotherWalletAmount - wholesaleItemPrice;
 
-var myNewFriends = myAge / myNumberOfFriends;
-var brotherNewFriends = brotherAge / brotherNumberOfFriends;
+let myNewFriends = myAge / myNumberOfFriends;
+let brotherNewFriends = brotherAge / brotherNumberOfFriends;
 
-var myFullName = myFirstName + ' ' + myMidInitial + ' ' + maidenLastName;
-var brotherFullName = brotherFirstName + ' ' + brotherMidInitial + ' ' + adoptedLastName;
+const myFullName = myFirstName + ' ' + myMidInitial + ' ' + maidenLastName;
+const brotherFullName = brotherFirstName + ' ' + brotherMidInitial + ' ' + adoptedLastName;
 
-//printing out the variables on the webpage
+//printing out the variables on the webpage:
 
-console.log('Retail Item Price: $' + retailItemPrice);
-console.log('Wholesale Item Price: $' + wholesaleItemPrice);
+console.log('Retail Item Price: $', retailItemPrice);
+console.log('Wholesale Item Price: $', wholesaleItemPrice);
 
-console.log('I have $' + myWalletAmount + ' in my wallet.');
-console.log('My brother has $' + brotherWalletAmount + ' in his wallet.');
+console.log('I have $', myWalletAmount, 'in my wallet.');
+console.log('My brother has $', brotherWalletAmount, 'in his wallet.');
 
-console.log('I bought the item from a retailer, now I have $' + newMyWalletAmount + ' in my wallet.');
-console.log('My brother bought the item from a wholesaler, now he has $' + newBrotherWalletAmount + ' in his wallet.');
+console.log('I bought the item from a retailer, now I have $', newMyWalletAmount, 'in my wallet.');
+console.log('My brother bought the item from a wholesaler, now he has $', newBrotherWalletAmount, 'in his wallet.');
 
-console.log('My name is ' + myFirstName + ', my middle initial is ' + myMidInitial + ', and my last name is ' + maidenLastName + '.');
+console.log('My name is', myFirstName, ', my middle initial is', myMidInitial, ', and my last name is', maidenLastName, '.');
 console.log('So my full name is ' + myFullName + ".");
 
-console.log('My brothers name is ' + brotherFirstName + ', his middle initial is ' + brotherMidInitial + ", and he sometimes goes by the last name " + adoptedLastName + '.');
-console.log('So his full name is ' + brotherFullName + '.');
+console.log('My brothers name is', brotherFirstName, ', his middle initial is ' + brotherMidInitial + ", and he sometimes goes by the last name " + adoptedLastName + '.');
+console.log('So his full name is', brotherFullName, '.');
 
-console.log('I am ' + myAge + " years old and I have " + myNumberOfFriends + ' friends, which means I have made ' + myNewFriends + " new friends a year.");
-console.log('My brother is ' + brotherAge + ' years old and he has ' + brotherNumberOfFriends + ' friends, which means he makes ' + brotherNewFriends + ' new friends a year.');
+console.log('I am ', myAge ," years old and I have ", myNumberOfFriends, ' friends, which means I have made ', myNewFriends, " new friends a year.");
+console.log('My brother is ', brotherAge, ' years old and he has ', brotherNumberOfFriends, ' friends, which means he makes ', brotherNewFriends, ' new friends a year.');
